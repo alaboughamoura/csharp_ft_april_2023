cc03755ae12247be7ad1d82dffb91781e91582c9
John Doe <johndoe@example.com>
2023-04-14 11:27:42 +0100

    Algo SLL (delete & reverse*)

--- Algos/sll.js
+++ Algos/sll.js
@@ -1,7 +1,7 @@
 //  Singly Linked List
 
 class Node {
-    constructor(value){
+    constructor(value) {
         // Attributes
         this.value = value
         this.next = null
@@ -11,64 +11,64 @@ class SLL {
     constructor() {
         this.head = null
     }
-    isEmpty(){
-        if(this.head === null){
+    isEmpty() {
+        if (this.head === null) {
             return true
-        }else{
+        } else {
             return false
         }
     }
-    addToFront(value){
-        let newNode  = new Node(value)
-        if(this.isEmpty() == true){
+    addToFront(value) {
+        let newNode = new Node(value)
+        if (this.isEmpty() == true) {
             this.head = newNode
             return this
-        } else{
+        } else {
             newNode.next = this.head
             this.head = newNode
             return this
         }
     }
-    print(){
-        if(this.isEmpty()){
+    print() {
+        if (this.isEmpty()) {
             return false
-        }else{
+        } else {
             let runner = this.head
-            while(runner){
+            while (runner) {
                 console.log(runner.value);
                 runner = runner.next
             }
         }
     }
-    addToBack(value){
+    addToBack(value) {
         let newNode = new Node(value)
-        if(this.isEmpty){
+        if (this.isEmpty) {
             this.head = newNode
             return this
-        }else{
+        } else {
             let runner = this.head
-            while(runner.next!==null){
+            while (runner.next !== null) {
                 runner = runner.next
             }
             runner.next = newNode
             return this
         }
     }
-    removeAtFront(){
-        if (this.isEmpty()){
+    removeAtFront() {
+        if (this.isEmpty()) {
             return false
-        }else{
+        } else {
             this.head = this.head.next
             return this
         }
     }
-    removeAtBack(){
-        if(this.isEmpty()){
+    removeAtBack() {
+        if (this.isEmpty()) {
             return false
-        }else{
+        } else {
             let runner = this.head
-            while(runner.next ) {
-                if(runner.next.next !== null){
+            while (runner.next) {
+                if (runner.next.next !== null) {
                     runner = runner.next
                 }
                 runner.next = null
@@ -76,38 +76,56 @@ class SLL {
             }
         }
     }
-    size(){
-        if (this.isEmpty()){
+    size() {
+        if (this.isEmpty()) {
             return false
         } else {
-            let count  = 1 
-            let runner  = this.head
-            while (runner !==null){
-                count ++ 
+            let count = 1
+            let runner = this.head
+            while (runner !== null) {
+                count++
                 runner = runner.next
             }
             return count
         }
     }
-    find(value){
-        if(this.isEmpty()){
+    find(value) {
+        if (this.isEmpty()) {
             return false
-        }else {
+        } else {
             let runner = this.head
-            while(runner){
-                if(runner.value === value){
+            while (runner) {
+                if (runner.value === value) {
                     return true
-                } else{
+                } else {
                     runner = runner.next
                 }
             }
             return false
         }
     }
+    delete(value) {
+        if (this.isEmpty()) {
+            return false
+        } else {
+            let runner = this.head
+            while (runner.next) {
+                if (runner.next.value == value) {
+                    console.log(`runner : ${runner.value} , runner.next : ${runner.next.value} runner.next.next  : ${runner.next.next}`);
+                    runner.next = runner.next.next
+                    return this
+                } else {
+                    runner = runner.next
+                }
+            }
+        }
+    }
+    reverse() {
+    }
 }
-let nodeOne  = new Node(5)
-let nodeTwo  = new Node(10)
-let nodeThree  = new Node(-2)
+let nodeOne = new Node(5)
+let nodeTwo = new Node(10)
+let nodeThree = new Node(-2)
 nodeOne.next = nodeTwo
 nodeTwo.next = nodeThree
 sll = new SLL()
@@ -126,6 +144,9 @@ sll2 = new SLL()
 // sll.print()
 // sll.removeAtBack()
 // console.log("************************");
-// sll.print()
+sll.print()
 // console.log(sll.size())
-console.log(sll.find(0));
+// console.log(sll.find(0));
+sll.delete(-2)
+console.log("************************");
+sll.print()
